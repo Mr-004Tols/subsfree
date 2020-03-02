b7b77ca2cbad6e0d46bffa2177c056d1beef542e
Mr.004 <57171003+Mr-004Tols@users.noreply.github.com>
2020-03-02 21:11:28 +0700

    Add files via upload

---
+++ subs.sh
@@ -0,0 +1,109 @@
+#!/system/xbin/bash
+#Bersihkan Layar
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 1
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+toilet -f standard -F gay "SUBSCRIBER GRATIS"
+echo "\033[31;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+echo ""
+echo "Author : " Mr.004 / Kang Garoxx"
+" Team : " 004 CYEBER TEAM"
+echo " Kontak Me : " 081228244243"
+" Pesan : "Jangan Di Salah Gunakan Dan Disebar :) Karena Nanti Dosa Tanggung Sendiri:)"
+sleep 1
+echo "ThanksTo:"
+echo "Allmember 004 CYEBER TEAM"
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+echo ""
+sleep 3
+echo "Kumpulan Kumpulan SUBSCRIBER GRATIS"
+echo "Gunakan Pakai URL YT:"
+read p;
+
+echo "URL Sedang Di Proses"
+sleep 2
+echo "\033[37;1m█10%" 
+sleep 1
+echo "██20%" 
+sleep 1
+echo "███30%" 
+sleep 3
+echo "████40%" 
+sleep 3
+echo "█████BENTAR SINYAL LU LAG" 
+sleep 3
+echo "██████60%" 
+sleep 5
+echo "\033[31;1mURL Gagal Di Proses..""\033[32;1mSilahkan Masukkan Kembali URL Anda:"
+read p;
+sleep 3
+echo "Sedang Menuju Proses..."
+sleep 3
+echo "\033[37;1m█10%"
+sleep 2
+echo "██20%"
+sleep 5
+echo "███30%"
+sleep 3
+echo "████ADD WA 081228244243"
+sleep 3
+echo "█████50%"
+sleep 3
+echo "██████60%"
+sleep 3
+echo "███████70%"
+sleep 4
+echo "████████80%"
+sleep 3
+echo "█████████TUNGGU"
+sleep 4
+echo "██████████100%"
+sleep 3
+echo "\033[32;1mSilahkan Masukkan Jumlah SUBSCRIBER Yang Anda Mau:"
+read p;
+sleep 2
+echo "Sedang Menuju Proses..."
+echo "\033[37;1m█10%"
+sleep 1
+echo "██20%"
+sleep 1
+echo "███30%"
+sleep 3
+echo "████40%"
+sleep 3
+echo "█████50%"
+sleep 3
+echo "██████60%"
+sleep 3
+echo "███████70%"
+sleep 4
+echo "████████80%"
+sleep 3
+echo "█████████90%"
+sleep 4
+echo "██████████100% DONE"
+sleep 6
+echo "\033[34;1mJumlah Yang Anda Masukkan Sukses...""\033[32;1mSilahkan Tunggu 30 Menit..."
+sleep 2
+sleep 3
+echo "Ketik Exit Untuk Keluar"
+read p;
+sleep 2
+sleep 3
+sleep 4
+
+echo "Pesan:Jika Script Ini Tidak Berfungsi Pada Anda Berarti Ada Kesalahan Dalam URL ANDA"
+sleep 3
+echo "TerimaKasih Untuk Yang Memakai Script Ini"
+sleep 2
+echo "ThanksTo: 004 Cyber Team"
+sleep 2
+sleep 4
+exit
